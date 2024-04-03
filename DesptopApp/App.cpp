ebefc103c936b214bd1fc3415e4a505a4922b74d
Tom Holewijn <Tom.holewijn@protonmail.com>
2024-04-03 12:21:21 +0200

    no message

--- DesptopApp/App.cpp
+++ DesptopApp/App.cpp
@@ -1,7 +1,7 @@
 #include <windows.h>
 
-int WINAPI WinMin(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
-    MessageBox(NULL, "HELLO", "My first GUI", MB_OK);
+    MessageBox(NULL, "HELLO HOW ARE YOU", "My Scrum Board", MB_OK);
     return 0;
 }
