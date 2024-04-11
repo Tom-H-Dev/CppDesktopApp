7e48e5995dc98258bbb842821395f6881c0ecd10
Tom Holewijn <Tom.holewijn@protonmail.com>
2024-04-11 15:24:11 +0200

    completet tutorial 3

--- DesptopApp/App.cpp
+++ DesptopApp/App.cpp
@@ -3,12 +3,16 @@
 #define FILE_MENU_NEW 1
 #define FILE_MENU_OPEN 2
 #define FILE_MENU_EXIT 3
+#define CHANGE_TITLE 4
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
 void AddMenus(HWND);
+void AddControls(HWND);
 
 HMENU hMenu;
+
+HWND hEdit;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
@@ -23,7 +27,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
     if (!RegisterClassW(&wc))
         return -1;
 
-    CreateWindowW(L"myWindowClass", L"My Application", WS_OVERLAPPEDWINDOW | WS_VISIBLE,100,100, 600, 500, 
+    CreateWindowW(L"myWindowClass", L"My Application", WS_OVERLAPPEDWINDOW | WS_VISIBLE,100,100, 1200, 700, 
                   NULL, NULL, NULL, NULL);
     MSG msg = {0};
 
@@ -50,12 +54,18 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
                 case FILE_MENU_NEW:
                     MessageBeep(MB_ICONINFORMATION);
                     return 0;
+                case CHANGE_TITLE:
+                    wchar_t text[100];
+                    GetWindowTextW(hEdit, text, 100);
+                    SetWindowTextW(hWnd, text);
+                    return 0;
             }
 
 
             return 0;
         case WM_CREATE:
             AddMenus(hWnd);
+            AddControls(hWnd);
             return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
@@ -71,15 +81,22 @@ void AddMenus(HWND hWnd)
     HMENU hFileMenu = CreateMenu();
     HMENU hSubMenu = CreateMenu();
 
-    AppendMenu(hSubMenu, MF_STRING, NULL, "SubMenu Item");
+    AppendMenu(hSubMenu, MF_STRING, CHANGE_TITLE, "Change Title");
 
     AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, "New");
     AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hSubMenu, "Open SubMenu");
     AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
-    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, "Exit");
+    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, "Quit");
 
     AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, "File");
     AppendMenu(hMenu, MF_STRING, NULL, "Help");
 
     SetMenu(hWnd, hMenu);
+}
+
+//Add items to the screen
+void AddControls(HWND hWnd)
+{
+    CreateWindowW(L"static", L"Enter Text Here :", WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER, 50,50, 100, 50, hWnd, NULL, NULL, NULL);
+    hEdit = CreateWindowW(L"Edit", L"...", WS_VISIBLE | WS_CHILD | WS_BORDER| ES_MULTILINE | ES_AUTOVSCROLL, 50, 110, 100, 50, hWnd, NULL, NULL, NULL);
 }
